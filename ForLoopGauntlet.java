ffce09a82ed308af937f56434cf3fea3a811734a
SomeRandomKid <jaiden.lau@gompersstudent.org>
2017-07-17 14:31:30 -0700

    Add files via upload

---
+++ ForLoopGauntlet.java
@@ -0,0 +1,17 @@
+
+public class ForLoopGauntlet {
+	public static void main(String [] args){
+	for (int i = 0; i <= 100; i++) {	
+		System.out.println(i);
+		}
+	for (int j = 100; j >= 0; j--) {
+		System.out.println(j);
+		}
+	for (int k = 2; k <= 100; k += 2) {
+		System.out.println(k);
+		}
+	for (int l = 1; l <= 99; l += 2) {
+		System.out.println(l);
+		}
+	}
+}
